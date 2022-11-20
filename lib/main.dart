e0c18f2ed8d74d92388119d612b01058c0691c94
Nasim-Alizai <sayedhakimi449@gmail.com>
2022-11-20 15:08:22 +0430

    add something

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter App',
       home: MyHomePage(),
     );
