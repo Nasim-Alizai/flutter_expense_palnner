e6c097fb5642b9e7432c05fb45439bfce3317aa0
Nasim-Alizai <sayedhakimi449@gmail.com>
2022-11-21 12:59:01 +0430

    add image

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -25,18 +25,18 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   final List<Transactions> _userTransactions = [
-    Transactions(
-      id: 't1',
-      title: "new shoes",
-      amount: 99.9,
-      date: DateTime.now(),
-    ),
-    Transactions(
-      id: 't2',
-      title: "new book",
-      amount: 9.98,
-      date: DateTime.now(),
-    )
+    // Transactions(
+    //   id: 't1',
+    //   title: "new shoes",
+    //   amount: 99.9,
+    //   date: DateTime.now(),
+    // ),
+    // Transactions(
+    //   id: 't2',
+    //   title: "new book",
+    //   amount: 9.98,
+    //   date: DateTime.now(),
+    // )
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
--- lib/widgets/Transaction_list.dart
+++ lib/widgets/Transaction_list.dart
@@ -11,55 +11,72 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-              child: Row(
+        child: userTransactions.isEmpty
+            ? Column(
                 children: [
+                  Text("No transactions added yet !"),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      '\$${userTransactions[index].amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
-                      ),
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        userTransactions[index].title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(userTransactions[index].date),
-                        style: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 14,
-                        ),
-                      )
-                    ],
-                  )
                 ],
-              ),
-            );
-          },
-          itemCount: userTransactions.length,
-        ));
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Theme.of(context).primaryColor,
+                              width: 2,
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            '\$${userTransactions[index].amount}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              userTransactions[index].title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              DateFormat.yMMMd()
+                                  .format(userTransactions[index].date),
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+                itemCount: userTransactions.length,
+              ));
   }
 }
